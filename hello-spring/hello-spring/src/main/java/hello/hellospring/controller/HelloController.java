c931d5912687343ad6404d775f30f647ec985b14
Chandra1153 <pcy1153@naver.com>
2023-11-10 15:54:29 +0900

    정적 컨텐츠

--- hello-spring/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -11,4 +12,5 @@ public class HelloController {
         model.addAttribute("data", "hello!!");
         return "hello";
     }
+
 }
